577a87239446ad53ffb0b67876bb2e36c540d696
cyangorilla <carreira.lj@gmail.com>
2020-08-05 12:13:46 -0300

    More changes

---
+++ US_AdultContentRemover/US_AdultContentRemover.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         US_AdultContentRemover
+// @namespace    https://github.com/cyan-gorilla/
+// @version      0.3
+// @date         2020-08-05
+// @description  A tool to remove M and A submissions from NewGrounds profiles
+// @author       cyangorilla
+// @match        https://*.newgrounds.com/*
+// @grant        none
+// @license      MIT
+// @copyright    2020, cyangorilla (https://openuserjs.org/users/cyangorilla)
+// ==/UserScript==
