5668dfafc14c9f7a9fd7739fee72c48343228cdc
cyangorilla <carreira.lj@gmail.com>
2020-08-05 11:28:46 -0300

    First Commit

--- A-M remover from profiles.user.js
+++ A-M remover from profiles.user.js
@@ -4,26 +4,40 @@
 // @author       cyangorilla
 // @namespace    https://www.newgrounds.com/
 // @include      https://*.newgrounds.com/*
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
-// @match        https://www.tampermonkey.net/index.php?version=4.10&ext=dhdg&updated=true
+// @version      0.2
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    window.addEventListener("load", removeAdultContent);
-    window.addEventListener("scroll", removeAdultContent);
-    function removeAdultContent(){
-        console.log("removing dirty stuff");
-        getRatedArray("m").forEach(removeTile);
-        getRatedArray("a").forEach(removeTile);
-        function getRatedArray(rating){
-            return Array.prototype.slice.call(document.getElementsByClassName("rated-" + rating));
+	let initialRemoval = setTimeout(removeAdultContent,0)
+
+	const userGallery = document.getElementsByClassName("userbody-guts")[0];
+	const observerCfg = { childList: true, subtree: true }
+
+	const cleanGallery = function(mutationsList, observer){
+        if(adultContentIsFound){
+            removeAdultContent();
         }
-        function removeTile(item, index){
-            item.parentElement.parentElement.style.display = "none";
-        }
+	};
+
+	const observer = new MutationObserver(cleanGallery);
+	observer.observe(userGallery, observerCfg);
+
+	function adultContentIsFound(){
+		return (getRatedArray("m").forEach(removeTile).length > 0) ||
+		       (getRatedArray("a").forEach(removeTile).length > 0);
+	}
+    function getRatedArray(rating){
+			return Array.prototype.slice.call(document.getElementsByClassName("rated-" + rating));
     }
+    function removeTile(item, index){
+			item.parentElement.parentElement.style.display = "none";
+    }
+	function removeAdultContent(){
+		console.log("NG Userscript: Removing adult content.");
+		getRatedArray("m").forEach(removeTile);
+		getRatedArray("a").forEach(removeTile);
+    }
+
 })();
