3d95307d116fe0529c7369d3756e08d6c3aa533a
cyangorilla <carreira.lj@gmail.com>
2020-08-05 12:16:40 -0300

    Added download and update tags

--- US_AdultContentRemover/US_AdultContentRemover.user.js
+++ US_AdultContentRemover/US_AdultContentRemover.user.js
@@ -4,11 +4,13 @@
 // @version      0.3
 // @date         2020-08-05
 // @description  A tool to remove M and A submissions from NewGrounds profiles
-// @updateURL    https://raw.githubusercontent.com/cyan-gorilla/userscripts/master/US_AdultContentRemover.js
+// @updateURL https://github.com/cyan-gorilla/userscripts/raw/master/US_AdultContentRemover/US_AdultContentRemover.meta.js
+// @downloadURL https://github.com/cyan-gorilla/userscripts/raw/master/US_AdultContentRemover/US_AdultContentRemover.user.js
 // @author       cyangorilla
 // @match        https://*.newgrounds.com/*
 // @grant        none
 // @license      MIT
+// @copyright    2020, cyangorilla (https://openuserjs.org/users/cyangorilla)
 // ==/UserScript==
 
 (function() {
